905cdbb99a252ddd2a58dee239f82d83fe8562bc
Q-gabe <gabriel.ong@u.nus.edu>
2020-03-11 06:14:04 +0800

    Updates

---
+++ precache-manifest.93d133fd7dd7fe926ab0eb904f6af43e.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e94b54c21362eb758eca9ddc7222ac86",
+    "url": "/temperature-radial-slider/index.html"
+  },
+  {
+    "revision": "7fa5d09aca168bc8532d",
+    "url": "/temperature-radial-slider/static/css/main.eaece945.chunk.css"
+  },
+  {
+    "revision": "013fa32b86ed3ca0885d",
+    "url": "/temperature-radial-slider/static/js/2.994f96df.chunk.js"
+  },
+  {
+    "revision": "e468b20662ce7acec401182a9064b329",
+    "url": "/temperature-radial-slider/static/js/2.994f96df.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7fa5d09aca168bc8532d",
+    "url": "/temperature-radial-slider/static/js/main.457e3050.chunk.js"
+  },
+  {
+    "revision": "c99a7cc4a4ce386b467c",
+    "url": "/temperature-radial-slider/static/js/runtime-main.07339ccb.js"
+  },
+  {
+    "revision": "8bbb6c83c86c1c22cc84e42c8442698c",
+    "url": "/temperature-radial-slider/static/media/Carlito-Bold.8bbb6c83.ttf"
+  },
+  {
+    "revision": "470e931a6e8b8873c400768620eb6cd0",
+    "url": "/temperature-radial-slider/static/media/ColdFace.470e931a.svg"
+  },
+  {
+    "revision": "1bc2d9ab9088394a8d5aecdda5d99d68",
+    "url": "/temperature-radial-slider/static/media/HotFace.1bc2d9ab.svg"
+  },
+  {
+    "revision": "51cde14616375ffe6602c1572fbf7262",
+    "url": "/temperature-radial-slider/static/media/InnerFace.51cde146.svg"
+  },
+  {
+    "revision": "b4a6d684fc3af3be302ebdeee47ee8c0",
+    "url": "/temperature-radial-slider/static/media/Spoke.b4a6d684.svg"
+  },
+  {
+    "revision": "70f4b6a29f0c07d689b3f89deba2a02e",
+    "url": "/temperature-radial-slider/static/media/SunIcon.70f4b6a2.svg"
+  },
+  {
+    "revision": "21c3caeba79be5b32f44f0715411a839",
+    "url": "/temperature-radial-slider/static/media/ThermostatBG.21c3caeb.svg"
+  },
+  {
+    "revision": "56c5294d07e81ed7317da6d195ff4bb3",
+    "url": "/temperature-radial-slider/static/media/Thumb.56c5294d.svg"
+  },
+  {
+    "revision": "6ae86405365767c10f7e31039faab17d",
+    "url": "/temperature-radial-slider/static/media/WorkSans-SemiBold.6ae86405.ttf"
+  }
+]);
